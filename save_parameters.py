fc9920ffe9237824c8814dafe9828d2f98248ebf
liacov <lauraiacovissi@gmail.com>
2020-05-21 17:04:42 +0200

    high probability refinement on maxpred

--- save_parameters.py
+++ save_parameters.py
@@ -8,7 +8,7 @@ def main():
         if n == 10**3: x = 9e-3
         else: x = 5e-3
         # define probabilities of interest
-        prob = [ 8e-1, 7e-1, 6e-1, 1e-1, 1e-2, x,                         # connected regine (high - low)
+        prob = [ 8e-1, 7e-1, 6e-1, 1e-1, 1e-2, x,                         # connected regime (high - low)
                 1/(3*n) + (2*np.log(n))/(3*n), 2/(3*n) + np.log(n)/(3*n), # supercritical regime
                 1/(2*n), 1/(10*n) ]                                       # subscritical regime
         prob2 = np.arange(1e-1 - 0.05, 1e-2, -(1e-1 - 1e-2)/20)
@@ -31,7 +31,7 @@ def main():
     for n in N2:
         if n == 10**3: x = 9e-3
         else: x = 5e-3
-        prob = [ 8e-1, 7e-1, 6e-1, 1e-1, 1e-2, x,                         # connected regine (high - low)
+        prob = [ 8e-1, 7e-1, 6e-1, 1e-1, 1e-2, x,                         # connected regime (high - low)
                 1/(3*n) + (2*np.log(n))/(3*n), 2/(3*n) + np.log(n)/(3*n), # supercritical regime
                 1/(2*n), 1/(10*n) ]                                       # subscritical regime
 
